fdacc8492ba1e248321e963e0a26989434606b21
Edward Peters <edward.peters@intermedix.com>
2015-12-03 17:15:51 -0500

    Get tests running and ready for CI.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,21 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-symlink');
   grunt.loadNpmTasks('grunt-bower-requirejs');
 
-  grunt.registerTask('default', ['symlink', 'bower']);
+  grunt.registerTask('install', ['symlink', 'bower']);
+  grunt.registerTask('default', ['mochaTest']);
 };
---
+++ test/index.js
@@ -0,0 +1,7 @@
+// Disable console logging during tests
+//console.log = function() {};
+
+describe('model tests', function() {
+  require('./models/capsule.js');
+  require('./models/validatorTextCapsule.js');
+});
